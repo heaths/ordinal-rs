5333a88e70c28ebb08f3f3bc7b6ae00a3c17305b
Heath Stewart <heaths@outlook.com>
2024-10-18 16:00:38 -0700

    Add benchmarks for varying `ordinal` formatting

--- benches/ordinal.rs
+++ benches/ordinal.rs
@@ -1,29 +1,56 @@
 // Copyright 2020 Heath Stewart.
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
-use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
 use ordinal::Ordinal;
 
-pub fn suffix(c: &mut Criterion) {
-    let mut group = c.benchmark_group("suffix");
-    for i in [1usize, 2, 3, 12, 123_456_789] {
-        group.bench_with_input(BenchmarkId::new("numeric_modulo", i), &i, |b, i| {
-            b.iter(|| i.suffix())
+const INPUTS: &[usize] = &[1usize, 2, 3, 12, 123_456_789];
+
+pub fn fmt(c: &mut Criterion) {
+    let mut group = c.benchmark_group("fmt");
+    for i in INPUTS {
+        group.bench_with_input(BenchmarkId::new("ordinal", i), i, |b, i| {
+            b.iter(|| black_box(i.ordinal()))
         });
-        group.bench_with_input(BenchmarkId::new("string_ends_with", i), &i, |b, i| {
-            b.iter(|| suffix_ends_with(*i))
-        });
-        group.bench_with_input(BenchmarkId::new("string_i8_modulo", i), &i, |b, i| {
-            b.iter(|| suffix_i8_modulo(*i))
+        group.bench_with_input(BenchmarkId::new("string_match", i), i, |b, i| {
+            b.iter(|| black_box(ordinal_string_match(*i)))
         });
     }
     group.finish();
 }
 
-criterion_group!(ordinal, suffix);
+pub fn suffix(c: &mut Criterion) {
+    let mut group = c.benchmark_group("suffix");
+    for i in INPUTS {
+        group.bench_with_input(BenchmarkId::new("numeric_modulo", i), i, |b, i| {
+            b.iter(|| black_box(i.suffix()))
+        });
+        group.bench_with_input(BenchmarkId::new("string_ends_with", i), i, |b, i| {
+            b.iter(|| black_box(suffix_ends_with(*i)))
+        });
+        group.bench_with_input(BenchmarkId::new("string_i8_modulo", i), i, |b, i| {
+            b.iter(|| black_box(suffix_i8_modulo(*i)))
+        });
+    }
+    group.finish();
+}
+
+criterion_group!(ordinal, fmt, suffix);
 criterion_main!(ordinal);
 
+fn ordinal_string_match(i: usize) -> String {
+    let s = i.to_string();
+    let suffix = match s.bytes().last().unwrap() {
+        b'1' if !s.ends_with("11") => "st",
+        b'2' if !s.ends_with("12") => "nd",
+        b'3' if !s.ends_with("13") => "rd",
+        _ => "th",
+    };
+    format!("{}{}", s, suffix)
+}
+
 fn suffix_ends_with(i: usize) -> &'static str {
+    // https://github.com/gleich/ordinal/blob/a4bf9bdc37d05940f87d8ceea1c4b47cda0da5b4/src/lib.rs#L59-L70
     let s = i.to_string();
     if s.ends_with("1") && !s.ends_with("11") {
         "st"
@@ -37,6 +64,7 @@ fn suffix_ends_with(i: usize) -> &'static str {
 }
 
 fn suffix_i8_modulo(i: usize) -> &'static str {
+    // https://github.com/gleich/ordinal/blob/fbcd2bece0ad81ef1c2da695c8e70bd34be94622/src/lib.rs#L59-L86
     use std::str::FromStr;
 
     let str_repr = i.to_string();
