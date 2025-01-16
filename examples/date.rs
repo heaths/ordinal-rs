5e39d71009555ab13d06665162f4e53025c49925
Heath Stewart <heaths@outlook.com>
2025-01-16 00:28:00 -0800

    Add date example (#14)
    * Add date example
    
    * Fix lint issues

---
+++ examples/date.rs
@@ -0,0 +1,12 @@
+// Copyright 2023 Heath Stewart.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+// cspell:ignore datelike
+use chrono::{DateTime, Datelike as _, Local};
+use ordinal_trait::Ordinal as _;
+
+fn main() {
+    let now: DateTime<Local> = Local::now();
+    let format = format!("%A, %B %-d{}, %Y %-I:%M:%S %p", now.day().suffix());
+    println!("{}", now.format(format.as_ref()));
+}
